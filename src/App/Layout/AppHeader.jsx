9df1a7a94ff9d775f0e608e0826733547768948a
Marzena <marzielinska80@gmail.com>
2023-04-26 19:16:34 +0200

    Add logo

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './styles/header.css';
+import { Link } from 'react-router-dom';
 
 export function AppHeader() {
   return (
     <header>
       <div>
-        <div className="header-title">Tytuł nagłówka</div>
+        <Link to="/">
+          <Logo  /
+
+        </Link>
+        />
       </div>
     </header>
   );
